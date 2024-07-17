238fdc1b76766a74863f8cb649e26f7752c5d3d7
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-07-17 10:47:53 -0700

    Part1: Word Scramble

--- UIKitPro4/ViewController.swift
+++ UIKitPro4/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var useWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "tex") {
+            if let startWords = try? String(contentsOf: startWordsURL){
+                allWords = startWords.components(separatedBy: "\n")
+            }
+//        } else {
+//            allWords = ["Sothesom"]
+            // OR
+            if allWords.isEmpty {
+                allWords = ["Sothesom"]
+            }
+        }
+        
+        startGame()
+    }
+    
+    func startGame() {
+        // ساختن کلمه
+        title = allWords.randomElement()
+        // ذخیره کلمه
+        useWords.removeAll(keepingCapacity: true)
+        // لود کردن کلمات
+        tableView.reloadData()
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return useWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "word", for: indexPath)
+        cell.textLabel?.text = useWords[indexPath.row]
+        return cell
+        
+    }
 
 }
 
